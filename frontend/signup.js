f72e2cc19fccbb513aa78506b4e2f1717ba2ff7c
asaldhi <asaldhi@students.cs.ubc.ca>
2023-11-05 18:31:35 -0800

    Added signup input checking, added successful signup redirect

--- frontend/signup.js
+++ frontend/signup.js
@@ -1,21 +1,41 @@
 document.getElementById("newUserForm").addEventListener("submit", collectUserSignUpInfo);
 
 function collectUserSignUpInfo() {
+    let valid = true
+
     const username = document.getElementById("userName").value;
     const email = document.getElementById("userEmail").value;
     const birthdate = document.getElementById("userBirthDate").value;
     const password = document.getElementById("userPassword").value;
     const passwordCheck = document.getElementById("userPasswordCheck").value;
 
-    // Check password is not empty
-    if(password.length == 0) {
-        alert("Please enter a valid password")
+    // Check simple fields
+    if (username.length == 0) {
+        alert("Please enter a valid username")
+        valid = false
+    } else if(email.length == 0) {
+        alert("Please enter a valid email")
+        valid = false
+    } else if(birthdate.length == 0) {
+        alert("Please enter a valid birth date")
+        valid = false
     }
 
-    // Check passwords match
-    if (password != passwordCheck) {
+    // Check password is not empty, matches with confirmation
+    if (password.length == 0) {
+        alert("Please enter a valid password")
+        valid = false
+    } else if (password != passwordCheck) {
         alert("Passwords dont match")
+        valid = false
     }
 
     // Send data to database
+
+    // Validate user details
+
+    if (valid) {
+        this.action = "login_page.html"; // Change the form's action URL
+    }
+
 }
