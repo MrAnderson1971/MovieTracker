e1d21e9d56536212e70cbe1a4d2417a501d1f8e4
tbuckoll <tbuckoll@students.cs.ubc.ca>
2023-11-06 14:37:37 -0800

    Added function to insert new user

--- appController.js
+++ appController.js
@@ -3,4 +3,14 @@ const appService = require('./appService');
 
 const router = express.Router();
 
+router.post("/insert-user", async (req, res) => {
+    const { username, email, password, birthDate } = req.body;
+    const insertResult = await appService.insertUser(username, email, password, birthDate);
+    if (insertResult) {
+        res.json({ success: true });
+    } else {
+        res.status(500).json({ success: false });
+    }
+});
+
 module.exports = router;
