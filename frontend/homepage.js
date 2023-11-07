1c0fe313ad902f7f9ae8e6a3924d835876dd7076
MrAnderson1971 <mrthomasanderson37@gmail.com>
2023-11-07 15:30:30 -0800

    Completed dynamic user profiling, added sign out functionality

--- frontend/homepage.js
+++ frontend/homepage.js
@@ -1,23 +1,51 @@
-// window.addEventListener("unload", function () {
-//     localStorage.clear();
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
-    const uN = localStorage.getItem("userName");
-    const uP = localStorage.getItem("userPassword");
+    const lS = localStorage.getItem("loginStatus");
 
-    if(uN != null && uP != null) {
+    if(lS != null) {
+        changeHomeNavBar();
         changeHomePage();
     }
 });
 
-function changeHomePage() {
-    const ele = document.querySelector('.account_links');
-    ele.style.display = 'none';
+function changeHomeNavBar() {
+    const al = document.querySelector('.account_links');
 
-    const loc = document.querySelector('.aaa');
+    // Remove sign/signup links
+    const linkElements = al.querySelectorAll("li");
+    for (let i = 0; i < linkElements.length; i++) {
+        linkElements[i].style.display = "none";
+    }
+
+    // Create a new sign-out link anchor
+    const signOutLink = document.createElement("a");
+    signOutLink.textContent = "SIGN OUT";
+
+    // Add listener to clear local storage and redirect user to refreshed homepage upon signing out
+    signOutLink.addEventListener("click", function () {
+        localStorage.clear();
+        window.location.href = 'homepage.html';
+    });
+
+    // Add link anchor to list item
+    const liSO = document.createElement("li");
+    liSO.append(signOutLink);
+
+    // Add list item to list
+    al.appendChild(liSO);
+}
+
+function changeHomePage() {
+    // Remove 'Get started' button
+    const gs = document.querySelector('.join');
+    gs.style.display = 'none';
+
+    // Add custom welcome message
+    const loc = document.querySelector('.userDashboard');
     const header = document.createElement('h1');
-    header.append("Welcome " + localStorage.getItem("userName"));
+    header.append("welcome, " + localStorage.getItem("userName"));
     loc.append(header);
 }
+
+
+
 
--- frontend/login.js
+++ frontend/login.js
@@ -1,28 +1,21 @@
 document.getElementById("logInButton").addEventListener("click", loginUser);
 
-let userName = null
-let userPassword = null
-let loginStatus = null
-
 function loginUser() {
-    // Clear any prior login information stored
-    localStorage.clear();
-
     // Collect user login information
     const un = document.getElementById("loginUserName").value;
     const up = document.getElementById("loginUserPassword").value;
 
     // Check username and password with database to ensure that they are valid
+    // Dummy test
+    let loginStatus = (un == "user1"  && up == "password1");
 
-    const valid = true
+    if(loginStatus) {
+        localStorage.setItem("userName", un);
+        localStorage.setItem("userPassword", up);
+        localStorage.setItem("loginStatus", "true");
 
-    if(valid) {
-        userName = un;
-        userPassword = up;
-        localStorage.setItem("userName", userName);
-        localStorage.setItem("userPassword", userPassword);
         window.location.href = 'homepage.html'
     } else {
-        alert("NAY")
+        alert("Incorrect username or password")
     }
 }
