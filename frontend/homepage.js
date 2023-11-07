c701e0b376bb8de22a23c4066b0cf05fc842767e
MrAnderson1971 <mrthomasanderson37@gmail.com>
2023-11-06 23:20:51 -0800

    Added foundation for dynamic user profiling

--- frontend/homepage.js
+++ frontend/homepage.js
@@ -1,6 +1,23 @@
-//
-// document.getElementById("join-button").addEventListener("click", display);
-//
-// function display() {
-//     alert("HI");
-// }
+// window.addEventListener("unload", function () {
+//     localStorage.clear();
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const uN = localStorage.getItem("userName");
+    const uP = localStorage.getItem("userPassword");
+
+    if(uN != null && uP != null) {
+        changeHomePage();
+    }
+});
+
+function changeHomePage() {
+    const ele = document.querySelector('.account_links');
+    ele.style.display = 'none';
+
+    const loc = document.querySelector('.aaa');
+    const header = document.createElement('h1');
+    header.append("Welcome " + localStorage.getItem("userName"));
+    loc.append(header);
+}
+
