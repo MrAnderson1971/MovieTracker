5a3ebb285c333c59ea21dfd30c876f0f04414ed6
tbuckoll <tbuckoll@students.cs.ubc.ca>
2023-11-11 15:16:46 -0800

    Added login and update watchlist endpoints

--- appController.js
+++ appController.js
@@ -7,6 +7,16 @@ const appService = require('./appService');
 
 const router = express.Router();
 
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const insertResult = await appService.insertUser(username, password);
+    if (insertResult) {
+        res.json({ success: true });
+    } else {
+        res.status(500).json({ success: false });
+    }
+});
+
 router.post("/insert-user", async (req, res) => {
     const { username, email, password, birthDate } = req.body;
     const insertResult = await appService.insertUser(username, email, password, birthDate);
@@ -21,6 +31,16 @@ router.delete("/delete-watchlist", async (req, res) => {
     const { watchlistID } = req.body;
     const deleteResult = await appService.deleteWatchlist(watchlistID);
     if (deleteResult) {
+        res.json({ success: true });
+    } else {
+        res.status(500).json({ success: false });
+    }
+});
+
+router.post("/update-watchlist", async (req, res) => {
+    const { watchlistID, name, userID } = req.body;
+    const insertResult = await appService.updateWatchlist(watchlistID, name, userID);
+    if (insertResult) {
         res.json({ success: true });
     } else {
         res.status(500).json({ success: false });
