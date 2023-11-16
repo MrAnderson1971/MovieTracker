fe43f2d67b253f598f434dbe69dd3106fd241c51
MrAnderson1971 <mrthomasanderson37@gmail.com>
2023-11-15 16:53:47 -0800

    login

--- appController.js
+++ appController.js
@@ -10,11 +10,11 @@ const router = express.Router();
 
 router.post("/login", async (req, res) => {
     const { username, password } = req.body;
-    const loginResult = await appService.getUser(username, password);
-    if (insertResult > 0) {
+    const loginResult = await appService.login(username, password);
+    if (loginResult > 0) {
         res.json({ success: true, userID: loginResult });
     } else {
-        res.status(500).json({ success: false, userID: loginResult });
+        res.status(401).json({ success: false, userID: loginResult });
     }
 });
 
@@ -53,13 +53,13 @@ router.get('/count-watchlist', async (req, res) => {
     const { userID } = req.body;
     const tableCount = await appService.countWatchlist(userID);
     if (tableCount >= 0) {
-        res.json({ 
-            success: true,  
+        res.json({
+            success: true,
             count: tableCount
         });
     } else {
-        res.status(500).json({ 
-            success: false, 
+        res.status(500).json({
+            success: false,
             count: tableCount
         });
     }
@@ -69,13 +69,13 @@ router.get('/count-movies', async (req, res) => {
     const { userID } = req.body;
     const tableCount = await appService.countMovies(userID);
     if (tableCount >= 0) {
-        res.json({ 
-            success: true,  
+        res.json({
+            success: true,
             count: tableCount
         });
     } else {
-        res.status(500).json({ 
-            success: false, 
+        res.status(500).json({
+            success: false,
             count: tableCount
         });
     }
@@ -85,13 +85,13 @@ router.get('/count-series', async (req, res) => {
     const { userID } = req.body;
     const tableCount = await appService.countSeries(userID);
     if (tableCount >= 0) {
-        res.json({ 
-            success: true,  
+        res.json({
+            success: true,
             count: tableCount
         });
     } else {
-        res.status(500).json({ 
-            success: false, 
+        res.status(500).json({
+            success: false,
             count: tableCount
         });
     }
@@ -101,13 +101,13 @@ router.get('/count-reviews', async (req, res) => {
     const { userID } = req.body;
     const tableCount = await appService.countReviews(userID);
     if (tableCount >= 0) {
-        res.json({ 
-            success: true,  
+        res.json({
+            success: true,
             count: tableCount
         });
     } else {
-        res.status(500).json({ 
-            success: false, 
+        res.status(500).json({
+            success: false,
             count: tableCount
         });
     }
@@ -117,13 +117,13 @@ router.get('/count-services', async (req, res) => {
     const { userID } = req.body;
     const tableCount = await appService.countServices();
     if (tableCount >= 0) {
-        res.json({ 
-            success: true,  
+        res.json({
+            success: true,
             count: tableCount
         });
     } else {
-        res.status(500).json({ 
-            success: false, 
+        res.status(500).json({
+            success: false,
             count: tableCount
         });
     }
@@ -133,13 +133,13 @@ router.get('/search-services', async (req, res) => {
     const { name, country, order } = req.body;
     const response = await appService.searchServices(name, country, order);
     if (tableCount >= 0) {
-        res.json({ 
-            success: true,  
+        res.json({
+            success: true,
             result: response
         });
     } else {
-        res.status(500).json({ 
-            success: false, 
+        res.status(500).json({
+            success: false,
             result: response
         });
     }
