9e5abc86becbabc8b4fd97febbe1e6a17861d324
MrAnderson1971 <mrthomasanderson37@gmail.com>
2023-11-24 18:45:55 -0800

    Updated navbar(s) with open admin page

---
+++ frontend/admin.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const lS = localStorage.getItem("loginStatus");
+
+    if(lS == null) {
+        window.location.href = 'signup.html';
+    } else {
+        changeAdminNavBar();
+    }
+});
+
+function changeAdminNavBar() {
+    const al = document.querySelector('.account_links');
+
+    // Remove sign/signup links
+    const linkElements = al.querySelectorAll("li");
+    for (let i = 0; i < linkElements.length; i++) {
+        linkElements[i].style.display = "none";
+    }
+
+    // Create a new sign-out link anchor
+    const signOutLink = document.createElement("a");
+    signOutLink.textContent = "SIGN OUT";
+
+    // Add listener to clear local storage and redirect user to refreshed homepage upon signing out
+    signOutLink.addEventListener("click", function () {
+        localStorage.clear();
+        window.location.href = 'homepage.html';
+    });
+
+    // Add link anchor to list item
+    const liSO = document.createElement("li");
+    liSO.append(signOutLink);
+
+    // Add list item to list
+    al.appendChild(liSO);
+}
+
+
